88b53e8a3f8ea247c9bd82093fd8d2799a3709b9
Dylan Finn <dylanfinn@Dylans-MacBook-Pro.local>
2018-10-30 14:15:33 -0700

    refactored for optimization and modularity

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,13 +1,16 @@
-import sys
 import random
 
-words = open("/usr/share/dict/words", "r").read().split('\n')
-numWords = sys.argv[1]
-
-def getRandomWords(words, numWords):
+def getRandomWords(numWords):
+    """
+        Takes an argument(int) and returns that many random words
+    """
+    words = open("/usr/share/dict/words", "r").read().split('\n')
     newSentence = []
     for i in range(int(numWords)):
         newSentence.append(words[random.randint(0, len(words))] + " ")
     return(''.join(newSentence))
 
-print(getRandomWords())
+if __name__ == "__main__":
+    import sys
+    numWords = sys.argv[1]
+    print(getRandomWords(numWords))
