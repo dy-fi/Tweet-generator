215bcf821d4c2a464417d5bc02bd067daa7cb1ef
Dylan Finn <dylanfinn@Dylans-MacBook-Pro.local>
2018-11-08 13:58:45 -0800

    Refactorization and clean up

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -4,9 +4,9 @@ from collections import Counter
 
 
 def histogram(source_text):
-    """
-        Takes an argument(file) and returns the times each word is used
-    """
+"""
+    Takes an argument(file) and returns the times each word is used
+"""
     words = open(source_text, "r").read().split()
     histogram = Counter()
     for word in words:
@@ -32,15 +32,19 @@ def random_word(histogram):
 
 # much faster O(n) and stores 1 int
 def weighted_random(histogram, total):
+"""
+    Takes a histogram and returns a weighted random choice from it
+"""
     destination = random.randint(0, total)
     for word in histogram:
         destination = destination - histogram[word]
         if destination < 0:
             return word
 
+# modularity
 if __name__ == "__main__":
     art_of_war = histogram("artofwar.txt")
-    total = sum(art_of_war.values())
+    total = unique_words(art_of_war)
     count = 10000
     newList = []
     while(count > 0):
